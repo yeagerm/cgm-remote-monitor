03940e26019dd38e15897d05f96e460ce76a353b
Andy Low <42089621+AndyLow91@users.noreply.github.com>
2026-05-01 21:19:05 +0100

    Update setup script for Node 20

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-sudo apt update
-sudo apt install -y nodejs build-essential git
+sudo apt-get update
+sudo apt-get install -y nodejs build-essential git
 
 npm install
